8b30e5c7f3bb001af105068464b79d7e454b31b3
LinkanKumarRout <linkankumarrout@gmail.com>
2024-05-04 23:16:58 +0530

    learn basics of list and tuple

---
+++ basic.py
@@ -0,0 +1,36 @@
+# Data Structure
+# - A way to store and organize the data so that it can be accessed effectively.
+
+# There are 2 types of data structure available.....
+# Built-in
+# - List, Tuple, Set, Dictionary
+
+# User Defined
+# Stack, Queue, Linked List, Tree, Graph
+
+# List & Tuple
+'''
+# List
+- List is a collection of elements enclosed in a [].
+- it can have different types of element.
+- List is ordered. i.e [1,2,3,4,5] != [1,3,5,4,2]
+- List element can be accessed using indexing. (0-based indexing)
+
+lst = [12,3,24,54,34,33,22]
+print(lst[0])
+
+- list can be nested.
+- list are mutable
+- List are dynamic.
+
+# Tuple
+- Tuple is a collection of elements enclosed is a ()
+- it can be a collection of element separated with ,
+
+- empty tuple like this -
+a = tuple()
+for a single element tuple you have to give a comma otherwise it will treat that variable based on the type()
+a = (2,) if we don't give , it will be treated as an integer
+
+- Tuples are immutable
+'''
