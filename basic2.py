75c949294b5bba316715312e7fd2e737d4e01941
Linkan Kumar Rout <linkankumarrout@gmail.com>
2024-06-04 12:44:04 +0530

    added about stack basics

---
+++ basic2.py
@@ -0,0 +1,20 @@
+# Stack Data Structure
+'''
+Stack is a data structure where addition and deletion of data takes place at the same end (top)
+and the opposite end is called base.
+
+it follows LIFO or FILO
+'''
+# Basic operation performed on a stack
+'''
+1. push - add element
+2. pop - remove element
+3. peek or top - give top element
+4. isEmpty - tell stack is empty or not
+'''
+# application
+'''
+- reverse a string
+- expression evaluation like prefix to postfix
+- forward and backward feature in a browser
+'''
