df2f0d1cd76cdee286bc921ada0bf45170bfc304
Linkan Kumar Rout <linkankumarrout@gmail.com>
2024-10-28 23:16:34 +0530

    added function to add element to linked list

--- Linked List/double_linked_list.py
+++ Linked List/double_linked_list.py
@@ -57,3 +57,53 @@ class DoubleLinkedList:
             while n is not None:
                 print(n.data, "<--", end = " ")
                 n = n.pref
+    
+    # Insertion Operation
+    # insert when the linked list is empty
+    def add_when_empty(self, data):
+        if self.head is None:
+            new_node = Node(data)
+            self.head = new_node
+        else:
+            print("Linked List is not empty.")
+
+    # add element at the beginning
+    def add_begin(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.nref = self.head
+            self.head.pref = new_node
+            self.head = new_node
+
+    # add element at the end
+    def add_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.nref is not None:
+                n = n.nref
+            n.nref = new_node
+            new_node.pref = n
+
+dl1 = DoubleLinkedList()
+# add element when linked list is empty
+dl1.add_when_empty(100)
+# add element at the beginning
+dl1.add_begin(40)
+dl1.add_begin(30)
+dl1.add_begin(20)
+dl1.add_begin(10)
+# add element at the end
+dl1.add_end(50)
+dl1.add_end(60)
+dl1.add_end(70)
+dl1.add_end(80)
+print("Linked List in Forward Direction: ")
+dl1.print_LL_Forward()
+print("\n")
+print("Linked List in Backward Direction: ")
+dl1.print_LL_Backward()
