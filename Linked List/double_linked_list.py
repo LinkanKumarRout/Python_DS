4635b4452f43aaecb3447025af3549c8e60e3949
Linkan Kumar Rout <linkankumarrout@gmail.com>
2024-11-05 23:50:50 +0530

    added functions to perform deletion operation

--- Linked List/double_linked_list.py
+++ Linked List/double_linked_list.py
@@ -127,7 +127,64 @@ class DoubleLinkedList:
             new_node.nref = n
             if n.pref is not None: # if it is not first node
                 n.pref.nref = new_node
+            else:
+                self.head = new_node
             n.pref = new_node
+
+    # Deletion Operation
+    # delete element at the beginning
+    def delete_begin(self):
+        if self.head is None:
+            print("Linked List is empty cannot delete.")
+        elif self.head.nref is None:
+            self.head = None
+            print("Linked List is empty now after element deletion.")
+        else:
+            self.head = self.head.nref
+            self.head.pref = None
+
+    # delete element at the end
+    def delete_end(self):
+        if self.head is None:
+            print("Linked List is empty cannot delete.")
+        elif self.head.nref is None:
+            self.head = None
+            print("Linked List is empty now after element deletion.")
+        else:
+            n = self.head
+            while n.nref is not None:
+                n = n.nref
+            n.pref.nref = None
+
+    # delete element by value
+    def delete_node(self, x):
+        if self.head is None:
+            print("Linked List is empty cannot delete.")
+            return
+        if self.head.nref is None:
+            if self.head.data == x:
+                self.head = None
+                print("Linked List is empty now after element deletion.")
+            else:
+                print("Element is not present in the Linked List.")
+            return
+        if self.head.data == x:
+            self.head = self.head.nref
+            self.head.pref = None
+            return
+        n = self.head
+        while n.nref is not None:
+            if n.data == x:
+                break
+            n = n.nref
+        if n.nref is not None: # if it is not last node
+            n.pref.nref = n.nref
+            n.nref.pref = n.pref
+        else: # if it is last node
+            if n.data == x:
+                n.pref.nref = None
+            else:
+                print("Element is not present in the Linked List.")
 
 dl1 = DoubleLinkedList()
 # add element when linked list is empty
@@ -151,3 +208,22 @@ dl1.print_LL_Forward()
 print("\n")
 print("Linked List in Backward Direction: ")
 dl1.print_LL_Backward()
+print()
+# delete element at the beginning
+dl1.delete_begin()
+print()
+print("Linked List after deleting element at the beginning: ")
+dl1.print_LL_Forward()
+print()
+# delete element at the end
+dl1.delete_end()
+print()
+print("Linked List after deleting element at the end: ")
+dl1.print_LL_Forward()
+print()
+# delete a specific node by value
+dl1.delete_node(55)
+print()
+print("Linked List after deleting element by value: ")
+dl1.print_LL_Forward()
+print()
